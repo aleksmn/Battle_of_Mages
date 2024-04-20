929580f628c9bdfbb8c7bf36a597faa0c30c243c
aleksmn <al-mn@yandex.ru>
2024-04-20 13:54:25 +0300

    init

---
+++ main copy 2.py
@@ -0,0 +1,84 @@
+# Александр
+import random
+import pygame as pg
+import pygame_menu
+
+
+pg.init()
+
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 550
+
+CHARACTER_WIDTH = 300
+CHARACTER_HEIGHT = 375
+
+FPS = 60
+
+font = pg.font.Font(None, 40)
+
+def load_image(file, width, height):
+    image = pg.image.load(file).convert_alpha()
+    image = pg.transform.scale(image, (width, height))
+    return image
+
+
+def text_render(text):
+    return font.render(str(text), True, "black")
+
+
+
+
+# Создаем класс для игры
+class Game:
+    def __init__(self):
+
+        # Создание окна
+        self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pg.display.set_caption("Битва магов")
+
+        self.background = load_image("images/background.png", SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.foreground = load_image("images/foreground.png", SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        # Создаем объекты игроков
+        # ....
+
+        # Запускаем игру
+        self.clock = pg.time.Clock()
+        self.run()
+
+
+    def run(self):
+        while True:
+            self.event()
+            self.update()
+            self.draw()
+            self.clock.tick(FPS)
+
+
+    def event(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                quit()
+
+
+    def update(self):
+        ...
+
+    
+    def draw(self):
+        self.screen.blit(self.background, (0, 0))
+
+
+        # Отрисовка персонажей
+        # ....
+
+
+        # передний план
+        self.screen.blit(self.foreground, (0, 0))
+        pg.display.flip()
+
+
+
+# Точка входа в программу
+if __name__ == "__main__":
+    Game()
