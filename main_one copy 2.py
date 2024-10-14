e804e2704a26eb35573b599e14c54261613c7abc
aleksmn <al-mn@yandex.ru>
2024-10-14 19:02:34 +0300

    fix

--- main_one copy 2.py
+++ main_one copy 2.py
@@ -50,7 +50,105 @@ class MagicBall(pg.sprite.Sprite):
             if self.rect.right <= 0:
                 self.kill()
 
+class Enemy(pg.sprite.Sprite):
+    def __init__(self, folder):
+        super().__init__()
 
+        self.folder = folder
+        self.load_animations()
+
+        self.hp = 200
+
+        self.image = self.idle_animation_left[0]
+        self.current_image = 0
+        self.current_animation = self.idle_animation_left
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (SCREEN_WIDTH - 100, SCREEN_HEIGHT // 2)
+
+        self.timer = pg.time.get_ticks()
+        self.interval = 300
+        self.side = "left"
+        self.animation_mode = True
+
+        self.magic_balls = pg.sprite.Group()
+
+        self.attack_mode = False
+        self.attack_interval = 500
+
+        self.move_interval = 800
+        self.move_duration = 0
+        self.direction = 0
+        self.move_timer = pg.time.get_ticks()
+
+        self.charge_power = 0
+
+    def load_animations(self):
+        self.idle_animation_right = [load_image(f"images/{self.folder}/idle{i}.png", CHARACTER_WIDTH, CHARACTER_HEIGHT) for i in range(1, 4)]
+
+        self.idle_animation_left = [pg.transform.flip(image, True, False) for image in self.idle_animation_right]
+
+        self.move_animation_right = [load_image(f"images/{self.folder}/move{i}.png", CHARACTER_WIDTH, CHARACTER_HEIGHT) for i in range(1, 5)]
+        self.move_animation_left = [pg.transform.flip(image, True, False) for image in self.move_animation_right]
+
+        # Приседения
+        self.down_right = load_image(f"images/{self.folder}/down.png", CHARACTER_WIDTH, CHARACTER_HEIGHT)
+        self.down_left = pg.transform.flip(self.down_right, True, False)
+        # Атака
+        self.attack_right = load_image(f"images/{self.folder}/attack.png", CHARACTER_WIDTH, CHARACTER_HEIGHT)
+        self.attack_left = pg.transform.flip(self.attack_right, True, False)
+        # Атака
+        self.charge_right = load_image(f"images/{self.folder}/charge.png", CHARACTER_WIDTH, CHARACTER_HEIGHT)
+        self.charge_left = pg.transform.flip(self.charge_right, True, False)
+
+
+    def handle_movement(self):
+        if self.attack_mode:
+            return
+
+        now = pg.time.get_ticks()
+
+        if now - self.move_timer < self.move_duration:
+            self.animation_mode = True
+            self.rect.x += self.direction
+            self.current_animation = self.move_animation_left if self.direction == -1 else self.move_animation_right
+        else:
+            if random.randint(1, 100) == 1 and now - self.move_timer > self.move_interval:
+                self.move_timer = pg.time.get_ticks()
+                self.move_duration = random.randint(400, 1500)
+                self.direction = random.choice([-1, 1])
+            else:
+                self.animation_mode = True
+                self.current_animation = self.idle_animation_left if self.side == "left" else self.idle_animation_right
+
+    def handle_attack_mode(self, player):
+
+        if not self.attack_mode:
+            attack_probability = 1
+            
+            if player.charge_mode:
+                attack_probability += 2
+
+            if random.randint(1, 100) <= attack_probability:
+                self.attack_mode = True
+                self.charge_power = random.randint(1, 100)
+
+
+
+    def update(self):
+ 
+        self.handle_animation()
+        self.handle_movement()
+
+
+    def handle_animation(self):
+        if self.animation_mode and not self.attack_mode:
+            if pg.time.get_ticks() - self.timer > self.interval:
+                self.current_image += 1
+                if self.current_image >= len(self.current_animation):
+                    self.current_image = 0
+                self.image = self.current_animation[self.current_image]
+                self.timer = pg.time.get_ticks()
 
 
 class Player(pg.sprite.Sprite):
@@ -222,6 +320,7 @@ class Game:
 
         # Персонажи
         self.player = Player(folder="fire wizard")
+        self.enemy = Enemy(folder="earth monk")
 
 
 
@@ -241,6 +340,7 @@ class Game:
 
         # отрисовка персонажей
         self.screen.blit(self.player.image, self.player.rect)
+        self.screen.blit(self.enemy.image, self.enemy.rect)
 
         self.player.magic_balls.draw(self.screen)
 
@@ -250,6 +350,7 @@ class Game:
     def update(self):
         # обновряем состояние игры, вызывается каждый кадр
         self.player.update()
+        self.enemy.update()
 
         self.player.magic_balls.update()
 
